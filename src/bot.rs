d75df4cac507110042f808e3d6b1259c03e9841c
Tuetuopay <tuetuopay@me.com>
2020-11-03 00:22:18 +0100

    Scope reset command to current channel

--- src/bot.rs
+++ src/bot.rs
@@ -229,11 +229,31 @@ async fn cmd_show(ctx: &Context, msg: &Message) -> CommandResult {
 #[required_permissions(ADMINISTRATOR)]
 async fn cmd_reset(ctx: &Context, msg: &Message) -> CommandResult {
     let conn = ctx.data.write().await.get_mut::<PgPool>().unwrap().get()?;
+    let game = msg.game(&conn)?;
+    let (game, part) = match game {
+        Some((game, Some(part))) => (game, part),
+        Some(_) => {
+            no_participant(ctx, msg).await?;
+            return Ok(())
+        }
+        None => return Ok(()),
+    };
 
     match msg.content.split(' ').collect::<Vec<_>>().as_slice() {
         [_, "do"] => {
             let reset_id = Uuid::new_v4();
-            diesel::update(dsl::win.filter(dsl::reset.eq(false)))
+            let win_ids = par_dsl::participation
+                .filter(par_dsl::is_win.eq(true))
+                .filter(par_dsl::game_id.eq(&game.id))
+                .select(par_dsl::win_id)
+                .load::<Option<Uuid>>(&conn)?
+                .into_iter()
+                .filter_map(|id| id)
+                .collect::<Vec<_>>();
+            diesel::update(
+                dsl::win
+                    .filter(dsl::reset.eq(false))
+                    .filter(dsl::id.eq(diesel::dsl::any(win_ids))))
                 .set((dsl::reset.eq(true),
                       dsl::reset_at.eq(diesel::dsl::now),
                       dsl::reset_id.eq(reset_id)))
@@ -245,6 +265,8 @@ async fn cmd_reset(ctx: &Context, msg: &Message) -> CommandResult {
             use diesel::sql_types::Timestamptz;
             let resets = dsl::win
                 .select((dsl::reset_id, diesel::dsl::sql::<Timestamptz>("max(reset_at) as rst")))
+                .inner_join(par_dsl::participation)
+                .filter(par_dsl::game_id.eq(&game.id))
                 .filter(dsl::reset.eq(true))
                 .filter(diesel::dsl::sql("true group by reset_id"))
                 .order_by(diesel::dsl::sql::<Timestamptz>("rst"))
